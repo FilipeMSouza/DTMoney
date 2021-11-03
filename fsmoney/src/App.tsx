f76c5c1f56deb5324ea60fe991fe9e7e8ab23558
FilipeMSouza <filipe.souza1906@gmail.com>
2021-11-03 11:30:55 -0300

    Styled-components install

---
+++ fsmoney/src/App.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    color: #8257e6;
+    font-style: sans-serif;
+    font-size: 64px;
+`
+
+
+export function App() {
+  return (
+    <div className="App">
+        <Title>Hello World!</Title>
+    </div>
+  );
+}
+
+
